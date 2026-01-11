5a579f9170692fde8a87949428c7b76b33708e3e
ishaanbansal26 <ishanbansal2123@gmail.com>
2026-01-11 16:36:17 +0530

    Recently Updated

---
+++ WebApplicationPractice/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationPractice.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
